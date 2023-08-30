64ff9e99ba2ade5a72cec9a6975abb6e50b8eb40
Benjamin Cook <benjamincook1313@gmail.com>
2023-08-30 15:08:21 -0600

    first commit

---
+++ src/model.js
@@ -0,0 +1,87 @@
+import { Model, DataTypes } from 'sequelize';
+import util from 'util';
+import connectToDB from './db.js';
+
+export const db = await connectToDB('postgresql:///ratings');
+
+export class User extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+
+User.init(
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    modelName: 'user',
+    sequelize: db,
+  }
+);
+
+export class Movie extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+};
+
+Movie.init({
+  movieId: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  overview: {
+    type: DataTypes.TEXT
+  },
+  releaseDate: {
+    type: DataTypes.DATE
+  },
+  posterPath: {
+    type: DataTypes.STRING
+  },
+},{
+  modelName: "movie",
+  sequelize: db
+});
+
+export class Rating extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+};
+
+Rating.int({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  }
+  id: {
+    type: DataTypes
+  }
+  id: {
+    type: DataTypes
+  }
+},{
+  modelName: 'user',
+  sequelize: db,
+});
